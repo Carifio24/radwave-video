3010885d1b23cebddf1aaaa361abfbe2cb26b5ff
Carifio24 <carifio24@gmail.com>
2023-12-17 13:42:58 -0500

    Add negative-phase data files.

--- RadcliffeWave.js
+++ RadcliffeWave.js
@@ -130,7 +130,7 @@ function setupDustLayer() {
 
 function setupClusterLayers() {
   const promises = [];
-  for (let phase = 0; phase <= 100; phase++) {
+  for (let phase = -10; phase <= 100; phase++) {
     const p = fetch(`data/RW_cluster_oscillation_${phase}_updated_radec.csv`)
       .then(response => response.text())
       .then(text => text.replace(/\n/g, "\r\n"))
